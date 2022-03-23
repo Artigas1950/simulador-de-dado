3f888743c067185e6353a5e1450f34797f531952
juanpe <juancoiro17@gmail.com>
2022-03-23 13:33:01 -0300

    cambios

--- java.js
+++ java.js
@@ -1,6 +1,27 @@
 console.log("Simulador de dado")
 
-var num = Math.random()
-var num2 = num * 6 ;
-var dado = Math.ceil(num2);
-alert(dado)
+let a = prompt("Ingrese cuantos dados quiere tirar del 0 al 4")
+
+function azar()
+{
+    var num = Math.random()
+    var num2 = num * 6 ;
+    var dado = Math.ceil(num2);
+    alert(dado)
+}
+
+if (a>4)
+{
+    alert("numero incorrecto")
+}
+
+else if (a<=4)
+{
+    for(let x = 1; x <= a; x++)
+    {
+        azar()
+    }
+}
+
+
+
