ae913e5e8b36aa6213546ecb06742d05c8ad1475
juanpe <juancoiro17@gmail.com>
2022-04-03 19:56:49 -0300

    primer entrega

--- java.js
+++ java.js
@@ -1,15 +1,30 @@
 console.log("Simulador de dado")
 
-let a = prompt("Ingrese cuantos dados quiere tirar del 1 al 4, ingrese 0 para salir")
-const resultados = []
+let nombres = prompt("Ingrese su nombre:")
+let a = prompt("Ingrese cuantos dados quiere tirar del 1 al 4, ingrese 0 para salir");
+const resultados = [];
 
-function azar()
+
+class Jugador {
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+}
+const jugador1 = new Jugador(nombres)
+console.log(jugador1)
+
+class Dado {
+    constructor(){}
+    azar()
 {
     var num = Math.random()
     var num2 = num * 6 ;
     var dado = Math.ceil(num2);
     return dado
 }
+}
+const dado1 = new Dado();
+console.log(dado1)
 
 while(a!=0)
 {
@@ -22,15 +37,23 @@ else if (a<=4)
 {
     for(let x = 1; x <= a; x++)
     {
-        let y = azar()
+        let y = dado1.azar();
         alert(`dado ${x}: ${y}`);
-        resultados.push(x);
+        resultados.push(y);
     }
 }
+  
+
 a = prompt("Ingrese cuantos dados quiere tirar del 1 al 4, ingrese 0 para salir")
 }
 alert("Gracias por participar");
-console.log(resultados)
+console.log(resultados);
+
+const primerimpar = resultados.find(dado => dado % 2 !== 0);
+console.log(primerimpar);
+
+const pares = resultados.filter(dado => dado % 2 === 0);
+console.log(pares);
 
 
 
